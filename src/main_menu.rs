00669f3d48729035c9a0f32dde5192d042aae267
terni526 <246582847+terni526@users.noreply.github.com>
2026-04-04 21:47:53 -0400

    used Window for input dialog

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -1,13 +1,39 @@
 use std::path::PathBuf;
 
-use eframe::egui::{Button, Context, Image, Pos2, Rect, Vec2, Window, include_image};
+use eframe::egui::{Button, Context, Image, Panel, Pos2, Rect, TextEdit, Vec2, Window, include_image};
 
 use egui_file_dialog::FileDialog;
 
 #[derive(Default)]
+/// The main menu of the application.
+///
+/// When creating a new project, the user is prompted with a file dialog, then, a check is ran
+/// to see if the output isn't `None` to proceed with the input dialog to name the project.
 pub struct MainMenu {
     new_project_dialog: FileDialog,
     new_project_path: Option<PathBuf>,
+    new_project_name: String, 
+}
+
+fn get_name_of_new_project(context: &Context, new_project_name: &mut String) {
+    let new_project_name_dialog = Window::new("Project Name")
+        .collapsible(false)
+        .resizable(false)
+        .max_width(250.0)
+        .max_height(230.0);
+
+    new_project_name_dialog.show(context, |ui| {
+        let new_project_name_input = TextEdit::singleline(new_project_name);
+        new_project_name_input.show(ui);
+
+        let _ok_button = ui.button("Ok");
+        let _close_button = ui.button("Close");
+
+        // Panel::bottom("ok_close_options_new_project_name_input").show_inside(ui, |ui| {
+        //     let _ok_button = ui.button("Ok");
+        //     let _close_button = ui.button("Close");
+        // });
+    });
 }
 
 impl eframe::App for MainMenu {
@@ -19,9 +45,7 @@ impl eframe::App for MainMenu {
         let new_project_button_rectangle =
             Rect::from_min_size(Pos2::new(70.0, 250.0), Vec2::new(125.0, 50.0));
 
-        let new_project_button = ui.put(
-            new_project_button_rectangle, Button::new("New Project")
-        );
+        let new_project_button = ui.put(new_project_button_rectangle, Button::new("New Project"));
 
         if new_project_button.clicked() {
             self.new_project_dialog.pick_directory();
@@ -31,8 +55,10 @@ impl eframe::App for MainMenu {
             self.new_project_path = Some(new_project_path);
 
             println!("NEW PROJECT PATH: {:?}", self.new_project_path);
+        }
 
-            create_new_project(self.new_project_path.as_mut().unwrap(), ui);
+        if self.new_project_path != None {
+            get_name_of_new_project(ui, &mut self.new_project_name);
         }
 
         let open_project_button_rectangle =
@@ -56,16 +82,4 @@ impl eframe::App for MainMenu {
         // Add new dialogs below here, always at end.
         self.new_project_dialog.update(ui);
     }
-}
-
-fn create_new_project(_new_project_path: &mut PathBuf, context: &Context) {
-    Window::new("Dialog")
-        .collapsible(false)
-        .resizable(false)
-        .show(context, |ui| {
-            ui.label("Hello!");
-            if ui.button("OK").clicked() {
-                // close dialog
-            }
-        });
 }
